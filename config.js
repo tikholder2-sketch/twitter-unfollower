52d2e722eff5cb9d01d4b1dfaf3a00a51e80278b
tikholder2-sketch <tikholder2@gmail.com>
2025-08-24 19:52:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ const CONFIG = {
     
     // OAuth 2.0 URLs
     OAUTH: {
-        AUTHORIZE_URL: 'https://twitter.com/i/oauth2/authorize',
-        TOKEN_URL: 'https://api.twitter.com/2/oauth2/token',
-    },
+    AUTHORIZE_URL: 'https://twitter.com/i/oauth2/authorize',
+    TOKEN_URL: 'https://api.twitter.com/2/oauth2/token',
+},
     
     // API Endpoints
     ENDPOINTS: {
